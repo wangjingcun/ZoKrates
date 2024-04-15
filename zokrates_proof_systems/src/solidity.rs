a1e4d660f6d08f7f75d28803c75b1577a3d4a6a1
dark64 <darem966@gmail.com>
2024-04-15 14:36:02 +0200

    fix sizes in scalar_mul

--- zokrates_proof_systems/src/solidity.rs
+++ zokrates_proof_systems/src/solidity.rs
@@ -481,7 +481,7 @@ library Pairing {
         input[2] = s;
         bool success;
         assembly {
-            success := staticcall(sub(gas(), 2000), 7, input, 0x80, r, 0x60)
+            success := staticcall(sub(gas(), 2000), 7, input, 0x60, r, 0x40)
             // Use "invalid" to make gas estimation work
             switch success case 0 { invalid() }
         }
